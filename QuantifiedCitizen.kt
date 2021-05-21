d8bca74acc07028c51334851acd2bc32cc054fb9
Mu-Sheng Wu <musheng.wu@gmail.com>
2021-05-20 20:26:04 -0400

    Add comments.

--- QuantifiedCitizen.kt
+++ QuantifiedCitizen.kt
@@ -1,6 +1,7 @@
 class QuantifiedCitizen {
     companion object {
         fun compress(input: String): String {
+            // Remvoe numbers from the string, because they will be ignored by the rule.
             val purify = input.replace("[0-9]+".toRegex(), "")
             val sb = StringBuffer()
             var i = 0
@@ -9,6 +10,7 @@ class QuantifiedCitizen {
                 var count = 0
                 val ch = purify[i]
     
+                // Loop through the repeating chars and count size.
                 while (i <= purify.lastIndex && purify[i] == ch) {
                     count++
                     i++
