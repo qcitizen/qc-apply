837c28532ee5053b3570d66b4d28f8609ff04278
mparrotta <46535265+mparrotta@users.noreply.github.com>
2021-05-23 12:42:03 -0400

    Added Comments
    Added comments to the function and file. Added one extra test case for a little flair.

--- qc_application.py
+++ qc_application.py
@@ -1,5 +1,20 @@
+"""
+    Matthew Parrotta
+    Qualified Citizen Application
+    Senior Fullstack Developer
+    05/23/21
+"""
+
+
+"""
+    This function takes an alpha numeric string and compresses it per implementation specifications
+    INPUT: An alpha numeric string
+    OUTPUT: The compressed string
+"""
+
 def collapse_alpha_numeric_stirng(an_input):
 
+    #Base case of a string with length 1
     if len(an_input) == 1:
         return an_input
 
@@ -8,20 +23,24 @@ def collapse_alpha_numeric_stirng(an_input):
 
     for i in an_input:
 
+        #Try to type cast character as int, if this works we know the character is an int and we pass it
         try:
             int(i)
             continue
         except ValueError:
             pass
-
+        
+        #Ensure the first character of the string is added to the list
         if len(consecutive_letters) == 0:
             consecutive_letters.append(i)
 
         else:
 
+            #Append consecutive letters
             if i == consecutive_letters[0]:
                 consecutive_letters.append(i)
     
+            #Append to output. If it is a single character, do not include the number of repitions
             else:
                 if len(consecutive_letters) == 1:
                     output += consecutive_letters[0]
@@ -29,7 +48,8 @@ def collapse_alpha_numeric_stirng(an_input):
                     output += consecutive_letters[0] + str(len(consecutive_letters))
 
                 consecutive_letters = [i]
-              
+    
+    #Append the final list of consecutive letters to output outside of loop
     if len(consecutive_letters) == 1:
         output += consecutive_letters[0]
     else:
@@ -38,6 +58,9 @@ def collapse_alpha_numeric_stirng(an_input):
     return(output)
                 
 
+"""
+    Test cases for application
+"""
 example_1 = collapse_alpha_numeric_stirng("aaabccccdd")
 print(example_1)
 
@@ -52,4 +75,7 @@ print(example_4)
 
 example_5 = collapse_alpha_numeric_stirng("effeac01cb65c")
 print(example_5)
+
+example_6 = collapse_alpha_numeric_stirng("mattisagoodprogrammer")
+print(example_6)
 
