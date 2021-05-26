5339f0c18d50a25dfdf62502ed82e9def09503a8
Nisarg Vadgama <nisargvv@gmail.com>
2021-05-26 16:28:20 -0400

    Add condition to not display 1

--- solution.js
+++ solution.js
@@ -1,15 +1,26 @@
 let strCompression = (str) => {
   let output = "";
-  for(let i = 0; i < str.length; i++) {
-    let currentLetter = str[i]; // stores current letter
+  for (let i = 0; i < str.length; i++) {
+    // stores current letter
+    let currentLetter = str[i];
     let count = 1;
-    while(str[i+1] === currentLetter) { // checks if next letter is same as current letter
+    // checks if next letter is same as current letter
+    while (str[i + 1] === currentLetter) {
       count++;
       i++;
-      }
+    }
+    // condition to not display single letter number i.e. 1
+    if (count === 1) {
+      output += currentLetter;
+    } else {
       output += currentLetter + count;
+    }
   }
   return output;
 }
 
 console.log(strCompression("aaabccccdd"));
+console.log(strCompression("aaaaaffffffffffc"));
+console.log(strCompression("abcd"));
+console.log(strCompression("ccceee12eccceee"));
+console.log(strCompression("effeac01cb65c"));
