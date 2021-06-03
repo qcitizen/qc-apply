87877adcbf9f5afbcc04331332c119ca41cd3d5b
Hammad <hammad@tanbits.com>
2021-06-04 00:13:20 +0500

    Count and concat consecutive characters

--- lib/apply.ex
+++ lib/apply.ex
@@ -1,2 +1,20 @@
 defmodule Apply do
+  def compress(<<letter::bytes-size(1), rest::bytes>>)do
+  compress(rest,"",letter,1)
+  end
+  def compress(<<letter::bytes-size(1), rest::bytes>>, chars,last,count) when letter !== last and count <=1 do
+    compress(rest,chars<>last,letter,1)
+  end
+  def compress(<<letter::bytes-size(1), rest::bytes>>, chars,last,count) when letter !== last do
+    compress(rest,chars<>last<>to_string(count),letter,1)
+  end
+  def compress(<<letter::bytes-size(1), rest::bytes>>, chars,last,count) when letter === last do
+    compress(rest,chars,letter,count+1)
+  end
+  def compress("",chars,last,count) when count<=1 do
+    chars<>last
+  end
+  def compress("",chars,last,count)  do
+    chars<>last<>to_string(count)
+  end
 end
