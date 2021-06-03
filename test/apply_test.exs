00c647825a284bdc01ba20e0e7dc9cb838fb43f6
Hammad <hammad@tanbits.com>
2021-06-04 00:31:52 +0500

    write basic test case

--- test/apply_test.exs
+++ test/apply_test.exs
@@ -2,7 +2,25 @@ defmodule ApplyTest do
   use ExUnit.Case
   doctest Apply
 
-  test "greets the world" do
-    assert Apply.hello() == :world
+  test "compress aaabccccdd" do
+    assert Apply.compress("aaabccccdd") === "a3bc4d2"
+  end
+  test "compress aaaaaffffffffffc" do
+    assert Apply.compress("aaaaaffffffffffc") === "a5f10c"
+  end
+  test "compress abcd" do
+    assert Apply.compress("abcd") === "abcd"
+  end
+  test "compress ccceee12eccceee" do
+    assert Apply.compress("ccceee12eccceee") === "c3e4c3e3"
+  end
+  test "compress effeac01cb65c" do
+    assert Apply.compress("effeac01cb65c") === "ef2eac2bc"
+  end
+  test "compress empty" do
+    assert Apply.compress("") === ""
+  end
+  test "compress integer" do
+    assert Apply.compress(1) === 1
   end
 end
