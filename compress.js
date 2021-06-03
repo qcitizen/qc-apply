3490f0e58fa696f1668f0f78411513e0cae87b3c
Furqan Aziz <furqan@invozone.com>
2021-06-04 03:08:58 +0800

    handled character count with a linear iteration

--- compress.js
+++ compress.js
@@ -1,12 +1,32 @@
 const assert = require('assert').strict;
 
 function compress(str) {
-  return str;
+  const list = str.split('');
+  let count = 1;
+  let result = "";
+  let last = list[0];
+  for(let i = 1; i <= list.length; i++) {
+    if (last == list[i]) {
+      count++;
+      continue;
+    }
+
+    if(count == 1){
+      result += last;
+    } else {
+      result += last + count;
+    }
+
+    last = list[i];
+    count = 1;
+  }
+
+  return result;
 }
 
 assert.equal(compress("aaabccccdd"), "a3bc4d2", "Failed for a3bc4d2");
-assert.equal(compress("aaaaaffffffffffc"), "aaaaaffffffffffc", "Failed for aaaaaffffffffffc");
+assert.equal(compress("aaaaaffffffffffc"), "a5f10c", "Failed for aaaaaffffffffffc");
 assert.equal(compress("abcd"), "abcd", "Failed for abcd");
 assert.notEqual(compress("abcd"), "a1b1c1d1", "Failed for a1b1c1d1");
-assert.equal(compress("ccceee12eccceee"), "ccceee12eccceee", "Failed for ccceee12eccceee");
-assert.equal(compress("effeac01cb65c"), "effeac01cb65c", "Failed for effeac01cb65c");
+assert.equal(compress("ccceee12eccceee"), "c3e4c3e3", "Failed for ccceee12eccceee");
+assert.equal(compress("effeac01cb65c"), "ef2eac2bc", "Failed for effeac01cb65c");
