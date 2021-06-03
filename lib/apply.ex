ff38affccec009bc587b0d2e6294286e229062b2
Hammad <hammad@tanbits.com>
2021-06-04 00:16:57 +0500

    discarding integers

--- lib/apply.ex
+++ lib/apply.ex
@@ -2,11 +2,10 @@ defmodule Apply do
   def compress(<<letter::bytes-size(1), rest::bytes>>)do
   compress(rest,"",letter,1)
   end
-  def compress(<<letter::bytes-size(1), rest::bytes>>, chars,last,count) when letter !== last and count <=1 do
-    compress(rest,chars<>last,letter,1)
-  end
+
   def compress(<<letter::bytes-size(1), rest::bytes>>, chars,last,count) when letter !== last do
-    compress(rest,chars<>last<>to_string(count),letter,1)
+    {chars,last,count} = concat_string(letter,chars,last,count)
+    compress(rest,chars,last,count)
   end
   def compress(<<letter::bytes-size(1), rest::bytes>>, chars,last,count) when letter === last do
     compress(rest,chars,letter,count+1)
@@ -15,7 +14,16 @@ defmodule Apply do
     chars<>last
   end
   def compress("",chars,last,count)  do
-    chars<>last<>to_string(count)
+    {result,_,_}= concat_string("",chars,last,count)
+    result
+  end
+  def concat_string(letter,chars,last,count) do
+    with false <- is_integer?(letter),true <- count>1 do
+      {chars <> last <> to_string(count),letter,1}
+    else
+      false-> {chars <> last,letter,1}
+      true-> {chars,last,count}
+    end
   end
   defp is_integer?(data) do
     with data when is_number(data) <- String.to_integer(data) do
