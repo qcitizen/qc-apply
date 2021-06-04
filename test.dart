4ece0d16caab924adb8f0c79521bf4f4473cf71c
Duncan <duncanadjei@gmail.com>
2021-06-04 22:19:20 +0000

    Implement assertions for scenarios

---
+++ test.dart
@@ -0,0 +1,9 @@
+void main() {
+  test('Test Scenarios ', () {
+    expect("abcd", removeNumbers("1242342a345b2423c234dx"));
+    expect("a3bc4d2", compressor("aaabccccdd"));
+    expect("a5f10c", compressor("aaaaaffffffffffc"));
+    expect("abcd", compressor("abcd"));
+    expect("ef2eac2bc", compressor("effeac01cb65c"));
+  });
+}
