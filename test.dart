f9bafce03a520e15fdbca9f31267cb8c1a605c92
Duncan <duncanadjei@gmail.com>
2021-06-04 22:23:52 +0000

    Fix failing assertions

--- test.dart
+++ test.dart
@@ -1,6 +1,6 @@
 void main() {
   test('Test Scenarios ', () {
-    expect("abcd", removeNumbers("1242342a345b2423c234dx"));
+    expect("abcd", removeNumbers("1242342a345b2423c234d"));
     expect("a3bc4d2", compressor("aaabccccdd"));
     expect("a5f10c", compressor("aaaaaffffffffffc"));
     expect("abcd", compressor("abcd"));
