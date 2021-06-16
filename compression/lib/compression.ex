2eaf6ad01eb05056def15c0282efb90f11ec72c4
Michael M. Rosario <michael.rosario@semsee.com>
2021-06-16 00:33:58 -0400

    Minor changes

--- compression/lib/compression.ex
+++ compression/lib/compression.ex
@@ -11,19 +11,19 @@ defmodule Compression do
 
   ## Examples
       iex> Compression.compress("aaabccccdd")
-      :a3bc4d2
+      "a3bc4d2"
 
       iex> Compression.compress("aaaaaffffffffffc")
-      :a5f10c
+      "a5f10c"
 
       iex> Compression.compress("abcd")
-      :abcd
+      "abcd"
 
       iex> Compression.compress("ccceee12eccceee")
-      :c3e4c3e3
+      "c3e4c3e3"
 
       iex> Compression.compress("effeac01cb65c")
-      :ef2eac2bc
+      "ef2eac2bc"
   """
   def compress(input_string \\ "") do
     input_string
@@ -38,9 +38,9 @@ defmodule Compression do
 
     ## Examples
       iex> Compression.remove_numeric("ccceee12eccceee")
-      :ccceeeeccceee
+      "ccceeeeccceee"
   """
-  def remove_numeric(input) do
+  def remove_numeric(input \\ "") do
     input |> String.replace(~r/[^\D]/, "")
   end
 
@@ -53,9 +53,9 @@ defmodule Compression do
 
     ## Examples
       iex> Compression.group_repeating("ccceeeeccceee")
-      :["ccc", "eeee", "ccc", "eee"]
+      ["ccc", "eeee", "ccc", "eee"]
   """
-  def group_repeating(input) do
+  def group_repeating(input \\ "") do
     Regex.scan(~r/(.)(\1*)/, input)
     |> Enum.reduce([], fn x, acc ->
       acc ++ [List.first(x)]
