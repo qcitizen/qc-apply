3c1f110840ce7e34e5b63bbb38a3a2e08a73de74
Michael M. Rosario <michael.rosario@semsee.com>
2021-06-15 23:34:53 -0400

    Step 1: remove non-characters

--- compression/lib/compression.ex
+++ compression/lib/compression.ex
@@ -1,18 +1,43 @@
 defmodule Compression do
   @moduledoc """
-  Documentation for `Compression`.
+  This module handles all operations related to compressing
   """
 
   @doc """
-  Hello world.
+  Compress a given string.
+
+  Given an alphanumeric input this function compresses
+  the text string into a smaller string.
 
   ## Examples
+      iex> Compression.hello("aaabccccdd")
+      :a3bc4d2
 
-      iex> Compression.hello()
-      :world
+      iex> Compression.hello("aaaaaffffffffffc")
+      :a5f10c
 
+      iex> Compression.hello("abcd")
+      :abcd
+
+      iex> Compression.hello("ccceee12eccceee")
+      :c3e4c3e3
+
+      iex> Compression.hello("effeac01cb65c")
+      :ef2eac2bc
   """
-  def hello do
-    :world
+  def compress(input_string \\ "") do
+    input_string
+    |> remove_numeric()
+  end
+
+  @doc """
+  Given a text string remove all non-character entries
+
+    ## Examples
+      iex> Compression.hello("ccceee12eccceee")
+      :ccceeeeccceee
+  """
+  def remove_numeric(input) do
+    input |> String.replace(~r/[^\D]/, "")
   end
 end
