891b70fc87d4ef8b1a5360c8ec9cd2c0be15b835
carter <carter@vie.works>
2021-06-25 18:08:29 -0400

    Renamed Compress module to Compressor, filled out compress function.

--- lib/compress.ex
+++ lib/compress.ex
@@ -1,18 +1,47 @@
-defmodule Compress do
+defmodule Compressor do
   @moduledoc """
-  Documentation for `Compress`.
+  Compress is a small module to compress alpha-numeric strings
+  containing consecutive, duplicate letters.
   """
 
   @doc """
-  Hello world.
+  Compress alpha-numeric string in the format of
+  "aaabbbccc" down to "a3b3c3".
 
   ## Examples
 
-      iex> Compress.hello()
-      :world
+      iex> Compressor.compress("aaabbbccc")
+      "a3b3c3"
 
   """
-  def hello do
-    :world
+  def compress(string) when is_binary(string) do
+    string
+    # create a list of the individual characters
+    |> String.graphemes()
+    # Filter out numbers in the original string
+    |> Enum.filter(fn character ->
+      case Integer.parse(character) do
+        # is number, skip
+        {_, ""} -> false
+        # is text, keep
+        _ -> true
+      end
+    end)
+    # Group consecutive duplicates together
+    |> Enum.chunk_by(fn character -> character end)
+    # Create a list of lists, each with a letter 
+    # and it's consecutive duplicate count next to it
+    |> Enum.map(fn x ->
+      case Enum.count(x) do
+        # Just show the letter if there's only 1
+        1 -> [Enum.at(x, 0)]
+        # Otherwise show letter with count
+        _ -> [Enum.at(x, 0), Enum.count(x)]
+      end
+    end)
+    # Flatten the list so that it's not nested
+    |> List.flatten()
+    # Then we can join them together to create a string
+    |> Enum.join()
   end
 end
--- test/compress_test.exs
+++ test/compress_test.exs
@@ -1,28 +1,28 @@
-defmodule CompressTest do
+defmodule CompressorTest do
   use ExUnit.Case
-  doctest Compress
+  doctest Compressor
 
   test "compresses aaabccccdd to a3bc4d2" do
-    assert Compress.compress("aaabccccdd") == "a3bc4d2"
+    assert Compressor.compress("aaabccccdd") == "a3bc4d2"
   end
 
   test "aaaaaffffffffffc to a5f10c" do
-    assert Compress.compress("aaaaaffffffffffc") == "a5f10c"
+    assert Compressor.compress("aaaaaffffffffffc") == "a5f10c"
   end
 
   test "compresses abcd to abcd" do
-    assert Compress.compress("abcd") == "abcd"
+    assert Compressor.compress("abcd") == "abcd"
   end
 
   test "compresses ccceee12eccceee to c3e4c3e3" do
-    assert Compress.compress("ccceee12eccceee") == "c3e4c3e3"
+    assert Compressor.compress("ccceee12eccceee") == "c3e4c3e3"
   end
 
   test "compresses effeac01cb65c to ef2eac2bc" do
-    assert Compress.compress("effeac01cb65c") == "ef2eac2bc"
+    assert Compressor.compress("effeac01cb65c") == "ef2eac2bc"
   end
 
   test "compresses mattisagoodprogrammer to mat2isago2dprogram2er" do
-    assert Compress.compress("mattisagoodprogrammer") == "mat2isago2dprogram2er"
+    assert Compressor.compress("mattisagoodprogrammer") == "mat2isago2dprogram2er"
   end
 end
