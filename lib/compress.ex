c1e8d6f229b8b173bfad8bb8a820b5701cbfcf01
carter <carter@vie.works>
2021-06-25 17:47:38 -0400

    Initialized new mix project, added basic tests for compression.

---
+++ lib/compress.ex
@@ -0,0 +1,18 @@
+defmodule Compress do
+  @moduledoc """
+  Documentation for `Compress`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Compress.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
