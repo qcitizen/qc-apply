575bdd531f6734f33db078fd2aadb13f580be238
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-07-08 02:43:13 +0530

    add-script-to-run-test

--- src/execute.sh
+++ src/execute.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-pipenv shell
 pipenv install
-python main.py '11233, aaabccccdd, aaaaaffffffffffc, abcd, ccceee12eccceee, effeac01cb65c, mattisagoodprogrammer'
+pipenv run python main.py '11233, aaabccccdd, aaaaaffffffffffc, abcd, ccceee12eccceee, effeac01cb65c, mattisagoodprogrammer'
---
+++ src/test.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+pipenv install
+pipenv run pytest
