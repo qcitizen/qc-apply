505341b4ba0967913eb7fef8a27e050b6805a6fe
Sofya <sofya.noskova@gmail.com>
2021-08-27 22:56:59 -0700

    dart boilerplate

---
+++ bin/main.dart
@@ -0,0 +1,3 @@
+import 'package:qc_apply/qc_apply.dart' as qc_apply;
+
+void main(List<String> arguments) {}
---
+++ test/qc_apply_test.dart
@@ -0,0 +1,4 @@
+import 'package:qc_apply/qc_apply.dart';
+import 'package:test/test.dart';
+
+void main() {}
