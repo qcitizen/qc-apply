f4ce2a491ac4abc8ba0643d5f5f3c4dd825c5aca
Sofya <sofya.noskova@gmail.com>
2021-08-27 23:54:53 -0700

    compress strings function implemented

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,16 @@
+import 'dart:io';
 import 'package:qc_apply/qc_apply.dart' as qc_apply;
 
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  print(
+    'Enter your alphanumeric string:',
+  );
+
+  String? initial = stdin.readLineSync(
+    retainNewlines: true,
+  );
+
+  print(
+    qc_apply.compressStr(initial),
+  );
+}
--- lib/qc_apply.dart
+++ lib/qc_apply.dart
@@ -1,1 +1,30 @@
+String? compressStr(String? initialStr) {
+  if (initialStr == null) return null;
 
+  if (initialStr == '') return '';
+
+  Map<int, String> chars =
+      initialStr.replaceAll(RegExp(r'[^a-zA-Z]'), '').split('').asMap();
+
+  String compressedStr = '';
+
+  int repeatCount = 1;
+
+  chars.forEach((idx, e) {
+    if (chars[idx] == chars[idx - 1]) {
+      repeatCount++;
+    } else {
+      if (repeatCount == 1) {
+        compressedStr += chars[idx].toString();
+      } else {
+        compressedStr += repeatCount.toString() + chars[idx].toString();
+        repeatCount = 1;
+      }
+    }
+    if (idx == chars.length - 1 && repeatCount > 1) {
+      compressedStr += repeatCount.toString();
+    }
+  });
+
+  return compressedStr;
+}
