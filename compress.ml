8bb137cdf02386cf45a3f6fe1905ef9eae002122
Shrikanth Upadhayaya <shrik450@gmail.com>
2021-09-02 08:28:10 +0530

    Implement transformation

--- compress.ml
+++ compress.ml
@@ -1,8 +1,37 @@
-type count = Single | Multiple of int
+type count =
+  | Single
+  | Multiple of int
 
 type pair = char * count
 
-let transform (str : string) : pair list = []
+let inc = function Single -> Multiple 2 | Multiple n -> Multiple (n + 1)
+
+let cmp (char_1 : char) ((char_2, count) : pair) =
+  match char_1 = char_2 with
+  | true ->
+      `Eq (char_1, count)
+  | false ->
+      `NonEq char_1
+
+
+let transform (str : string) : pair list =
+  let rec _transform str pos accum =
+    if pos = String.length str
+    then accum
+    else
+      let new_accum =
+        match cmp str.[pos] (List.hd accum) with
+        | `Eq (char, count) ->
+            let new_head = (char, inc count) in
+            new_head :: List.tl accum
+        | `NonEq char ->
+            (char, Single) :: accum
+      in
+      _transform str (pos + 1) new_accum
+  in
+  (* Bootstrap with the first character taken care of. *)
+  _transform str 1 [ (str.[0], Single) ]
+
 
 let generate (lst : pair list) : string = ""
 
