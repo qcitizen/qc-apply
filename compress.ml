1dd97fd8e549ace0a4742cc3e236335004fc6702
Shrikanth Upadhayaya <shrik450@gmail.com>
2021-09-02 08:11:09 +0530

    Scaffold solution

--- compress.ml
+++ compress.ml
@@ -1,1 +1,9 @@
-let compress str : string = str
+type count = Single | Multiple of int
+
+type pair = char * count
+
+let transform (str : string) : pair list = []
+
+let generate (lst : pair list) : string = ""
+
+let compress str : string = str |> transform |> generate
