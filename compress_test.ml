2ec0873d1ff2aa42af8ef65f326dc49e25ac370f
Shrikanth Upadhayaya <shrik450@gmail.com>
2021-09-02 08:37:22 +0530

    Add test cases from README

--- compress_test.ml
+++ compress_test.ml
@@ -1,12 +1,23 @@
 open Compress
 open OUnit2
 
-let test_case input expectation _ =
+let test_case input expectation =
+  input
+  >:: fun _ ->
   let res = compress input in
   assert_equal expectation res
 
-let one = "first case" >:: test_case "aaabccccdd" "a3bc4d2"
 
-let suite = "Test Compress" >::: [ one ]
+let one = test_case "aaabccccdd" "a3bc4d2"
+
+let two = test_case "aaaaaffffffffffc" "a5f10c"
+
+let three = test_case "abcd" "abcd"
+
+let four = test_case "ccceee12eccceee" "c3e4c3e3"
+
+let five = test_case "effeac01cb65c" "ef2eac2bc"
+
+let suite = "Test Compress" >::: [ one; two; three; four; five ]
 
 let () = run_test_tt_main suite
