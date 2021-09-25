2008f3bf83aeaf3ae399f2925afef454202ac52c
Aaron Surty <aarons@actionsoftware.com>
2021-09-25 08:08:29 -0400

    adding base function that compresses alphanumeric

---
+++ compressor/lib/string_util.dart
@@ -0,0 +1,9 @@
+/*
+ * Author: Aaron Surty
+ * A utility that compresses an alphanumeric string by collapsing consecutive characters.  The result of a compress/decompress would result in the loss of numbers.
+ * Input : String (input) - an alphanumeric string
+ * Output : String - the compressed string with numbers representing how many adjacent consecutive characters there are
+ */
+String compressAlphaNumeric(String input) {
+  return '';
+}
