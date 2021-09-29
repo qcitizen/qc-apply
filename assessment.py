2c2c7003d1087340f5827978b4cc1814ed6591dd
narendranadh14 <91559257+narendranadh14@users.noreply.github.com>
2021-09-28 22:58:45 -0400

    Added new function that compresses an alphanumeric string

--- assessment.py
+++ assessment.py
@@ -1,1 +1,14 @@
+new_list = []
 
+my_string = input("Enter a string: ")
+x = ''.join(sorted(set(my_string), key=my_string.index))
+test_str = my_string
+
+for i in x:
+    counter = test_str.count(i)
+    if i == ' ':
+        i = 'Space'
+    if counter == 1:
+	    counter = ''
+    new_list.append(i + str(counter))
+print("final result: %s "%(''.join(new_list)))
