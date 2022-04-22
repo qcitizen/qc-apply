86b15eba0fe6aae59320ca715d18af59b355c7d7
Maryam Yusuf <maryam.m.yusuf1802@gmail.com>
2022-04-22 17:29:13 +0100

    add a check to exclude numbers from getting encoded

--- compresser.py
+++ compresser.py
@@ -2,17 +2,26 @@ def encode(string):
     compressed_string = []
     current_length = 1
     previous_index = 0
+    jump = 1
+    reset_jump = False
 
     for i in range(1, len(string)):
         current_char = string[i]
         previous_char = string[previous_index]
+        if current_char.isdigit():
+            jump += 1
+            continue
+        else:
+            reset_jump = True
 
         if current_char != previous_char:
             append_character(current_length, previous_char, compressed_string)
             current_length = 0
 
         current_length += 1
-        previous_index += 1
+        previous_index += 1 if jump == 1 else + jump
+        if reset_jump:
+            jump = 1
 
     return "".join(compressed_string)
 
