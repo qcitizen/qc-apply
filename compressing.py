66a3b3b266fab48edda52d8d0c2953f66528eb31
Mikhail Varantsou <mikhail.varantsou@rigelstorm.com>
2022-06-14 23:41:37 +0200

    Add test for compression function

--- compressing.py
+++ compressing.py
@@ -10,10 +10,20 @@ logger = logging.getLogger(__name__)
 
 
 def _test():
-    ...
+    expected = {
+        'aaabccccdd': 'a3bc4d2',
+        'aaaaaffffffffffc': 'a5f10c',
+        'abcd': 'abcd',
+        'ccceee12eccceee': 'c3e4c3e3',
+        'effeac01cb65c': 'ef2eac2bc',
+    }
+    for input_str, expected_output in expected.items():
+        output = compress(input_str)
+        msg = f'Got {output} for input {input_str}, expected {expected_output}'
+        assert output == expected_output, msg
 
 
-def compress():
+def compress(input_str):
     ...
 
 
