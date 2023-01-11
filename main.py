d2fc2ae8fc9fb9a130002e688e1154be545d7862
apinanyogaratnam <apinanapinan@icloud.com>
2023-01-10 23:46:08 -0500

    refactor: removed debug messages

--- main.py
+++ main.py
@@ -22,10 +22,7 @@ def compress(message: str) -> str:
         if letter.isdigit():
             continue
 
-        print(f'letter: {letter}')
-        print(f'previous letter: {previous_letter}')
         if previous_letter and previous_letter != letter:
-            print(f'new letter appearing: {letter}')
             current_letter_count = letter_counts.get(previous_letter, 0)
             compressed_message += f'{previous_letter}{current_letter_count}' if current_letter_count > 1 else previous_letter
             del letter_counts[previous_letter]
@@ -33,12 +30,9 @@ def compress(message: str) -> str:
         else:
             # previous_letter is equivalent to letter
             letter_counts[letter] += 1
-            print(f'appending letter to count: {letter_counts.get(letter)}')
 
         previous_letter = letter
 
-    print(f'last letter: {previous_letter}')
-    print(f'last letter count: {letter_counts.get(letter)}')
     compressed_message += f'{previous_letter}{letter_counts.get(letter)}' if current_letter_count > 1 else previous_letter
 
     return compressed_message
