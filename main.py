4148f867d0a8cc532ed9250b66a1c7604d554911
apinanyogaratnam <apinanapinan@icloud.com>
2023-01-10 23:14:14 -0500

    feat: implemented compress function

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def compress(message: str) -> str:
     """Compress an alphanumeric  by collapsing consecutive values.
 
@@ -12,4 +15,22 @@ def compress(message: str) -> str:
     >>> compress('aaaaaffffffffffc')
     a5f10c
     """
-    pass
+    letter_counts = defaultdict(int)
+    letter_order = ""
+    for letter in message:
+        if not letter.isdigit():
+            letter_counts[letter] += 1
+
+            if letter not in letter_order:
+                letter_order += letter
+
+    compressed_message = ""
+    for letter in letter_order:
+        letter_count += letter_counts.get(letter)
+
+        if not letter_count:
+            raise ValueError('Invalid letter')
+
+        compressed_message += f'{letter}{letter_count}' if letter_count > 1 else letter
+
+    return compressed_message
