cef29d498cbcc6895b6eb7c3e94d3b5ae6c25c94
Josh Dutcher <1928341+joshdutcher@users.noreply.github.com>
2023-10-16 14:33:47 -0500

    open file and scan; remove numeric characters

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 )
 
 func main() {
+	// make sure the command has been run properly and a file to parse has been provided
 	if len(os.Args) != 2 {
 		fmt.Println(`USAGE:
 	main.go requires one argument which is the name of the file to be parsed.
@@ -24,4 +27,16 @@ func main() {
 		log.Fatal(err)
 	}
 	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		initialString := scanner.Text()
+
+		// remove digits
+		regex := regexp.MustCompile("[0-9]+")
+		parsedString := regex.ReplaceAllString(initialString, "")
+
+		fmt.Println(initialString + " → " + parsedString)
+	}
 }
