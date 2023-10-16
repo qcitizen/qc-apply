92d411088f808bfd2497a2eafccc7a62de1c4b80
Josh Dutcher <1928341+joshdutcher@users.noreply.github.com>
2023-10-16 16:34:39 -0500

    write tests, refactor a bit to make more test friendly

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -11,24 +12,37 @@ import (
 	"unicode"
 )
 
+var errUsage = errors.New("Usage error")
+var errIllegalCharacter = errors.New("Illegal character")
+var errFile = errors.New("Error opening file")
+
 func main() {
+	err := compressStrings()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func compressStrings() error {
 	// make sure the command has been run properly and a file to parse has been provided
 	if len(os.Args) != 2 {
 		fmt.Println(`USAGE:
-	main.go requires one argument which is the name of the file to be parsed.
-	main.go only looks for that file in the current directory.
+		main.go requires one argument which is the name of the file to be parsed.
+		main.go only looks for that file in the current directory.
 
-	E.g.:
-	$ go run main.go <filename>`)
-		return
+		E.g.:
+		$ go run main.go <filename>`)
+		return errUsage
 	}
 
 	filename := os.Args[1]
 	file, err := os.Open(filename)
 	if err != nil {
-		// could output a cleaner error message here but console output is sufficient for now
-		log.Fatal(err)
+		// could output a cleaner error message here but default output is sufficient for now
+		fmt.Println(err.Error())
+		return errFile
 	}
+
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
@@ -36,41 +50,53 @@ func main() {
 	for scanner.Scan() {
 		initialString := scanner.Text()
 
-		// remove digits
-		regex := regexp.MustCompile("[0-9]+")
-		parsedString := regex.ReplaceAllString(initialString, "")
-
-		// count repeated characters
-		var builder strings.Builder
-		var character rune
-		var charCount = 0
-		for _, c := range parsedString {
-			// ensure the character is alphabetic as expected
-			if !unicode.IsLetter(c) {
-				log.Fatal("Illegal character detected in " + filename + " (alphanumeric characters only)")
-				return
-			}
-
-			if c == character {
-				charCount++
-			} else {
-				builder = buildString(builder, character, charCount)
-
-				character = c
-				charCount = 1
-			}
+		parsedString, err := parseString(initialString)
+		if err != nil {
+			return err
 		}
-		builder = buildString(builder, character, charCount)
-		parsedString = builder.String()
 
 		fmt.Println(initialString + " → " + parsedString)
 	}
+
+	return nil
+}
+
+func parseString(initialString string) (string, error) {
+	// remove digits
+	regex := regexp.MustCompile("[0-9]+")
+	parsedString := regex.ReplaceAllString(initialString, "")
+
+	// count repeated characters
+	var builder strings.Builder
+	var character rune
+	var charCount = 0
+	for _, c := range parsedString {
+		// ensure the character is alphabetic as expected
+		if !unicode.IsLetter(c) {
+			fmt.Println("Illegal character \"" + string(c) + "\" found. (alphanumeric characters only)")
+			return "", errIllegalCharacter
+		}
+
+		if c == character {
+			charCount++
+		} else {
+			builder = buildString(builder, character, charCount)
+
+			character = c
+			charCount = 1
+		}
+	}
+	builder = buildString(builder, character, charCount)
+
+	return builder.String(), nil
 }
 
 func buildString(builder strings.Builder, r rune, count int) strings.Builder {
-	builder.WriteRune(r)
-	if count > 1 {
-		builder.WriteString(strconv.Itoa(count))
+	if unicode.IsPrint(r) { // prevents string builder from inserting null characters like "\x00"
+		builder.WriteRune(r)
+		if count > 1 {
+			builder.WriteString(strconv.Itoa(count))
+		}
 	}
 	return builder
 }
