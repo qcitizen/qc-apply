badebe26271358eabcd73a3008cc076662553792
Matthew Sumpter <matthew.sumpter@meowwolf.com>
2023-10-24 17:54:31 -0600

    Add test function and add test cases

--- main.ts
+++ main.ts
@@ -40,9 +40,37 @@ function compress(input: string): string {
     return noNumbers[0] ? process(noNumbers.slice(1), noNumbers[0], 1) : '';
 }
 
+/**
+ * Tests the compression function and logs the results along with a provided explanation.
+ *
+ * @example
+ * // Logs:
+ * // Test:  Basic compression test
+ * // Compressing 'aaabccccdd' -> Expected: 'a3bc4d2', Got: 'a3bc4d2' true
+ * testCompression('Basic compression test', 'aaabccccdd', 'a3bc4d2');
+ *
+ * @param {string} caseExplanation - A brief description or explanation of the test case.
+ * @param {string} input - The string to be compressed.
+ * @param {string} expected - The expected result after compression.
+ */
+function testCompression(caseExplanation: string, input: string, expected: string) {
+    const result = compress(input);
+    console.log("\nTest: ", caseExplanation)
+    console.log(`Compressing '${input}' -> Expected: '${expected}', Got: '${result}'`, result === expected);
+}
+
 // Test cases
-console.log(compress('aaabccccdd') === 'a3bc4d2');
-console.log(compress('aaaaaffffffffffc') === 'a5f10c');
-console.log(compress('abcd') === 'abcd');
-console.log(compress('ccceee12eccceee') === 'c3e4c3e3');
-console.log(compress('effeac01cb65c') === 'ef2eac2bc');
+console.log('Provided test cases:');
+testCompression('base case, ', 'aaabccccdd', 'a3bc4d2');
+testCompression('base case 2', 'aaaaaffffffffffc', 'a5f10c');
+testCompression('string with only single instance of each letter', 'abcd', 'abcd');
+testCompression('string with numbers', 'ccceee12eccceee', 'c3e4c3e3');
+testCompression('string with numbers 2', 'effeac01cb65c', 'ef2eac2bc');
+
+console.log('\nAdditional test cases:');
+testCompression('empty string', '', '');
+testCompression('string with only one character', 'a', 'a');
+testCompression('string with only one character and a number', 'a1', 'a');
+testCompression('string with only numbers', '123456789', '');
+testCompression('string with special characters', 'a!@aa$%b3^^bc', 'a3b2c')
+
