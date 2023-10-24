6be2ddc3cb83045c3cb88789a93a688e017e8b04
Matthew Sumpter <matthew.sumpter@meowwolf.com>
2023-10-24 17:38:02 -0600

    Initial implementation

--- main.js
+++ main.js
@@ -1,27 +1,42 @@
-function process(s: string, prevChar: string, count: number): string {
+/**
+ * Recursively processes a string to compress consecutive characters.
+ *
+ * @param {string} s - The string to process.
+ * @param {string} prevChar - The previous character that was processed.
+ * @param {number} count - The count of consecutive occurrences of `prevChar`.
+ * @returns {string} - The compressed string segment.
+ */
+function process(s, prevChar, count) {
     // Base case
     if (s.length === 0) {
         return count > 1 ? prevChar + count.toString() : prevChar;
     }
-
     // If the current character matches the previous one, increase count and continue
     if (s[0] === prevChar) {
         return process(s.slice(1), prevChar, count + 1);
-    } else {
+    }
+    else {
         // Add the previous char and its count (if > 1) to the result and reset the count
-        const result = count > 1 ? prevChar + count.toString() : prevChar;
+        var result = count > 1 ? prevChar + count.toString() : prevChar;
         return result + process(s.slice(1), s[0], 1);
     }
 }
-
-function compress(input: string): string {
+/**
+ * Compresses an alphanumeric string by removing numbers and then collapsing consecutive values.
+ *
+ * @example
+ * // Returns 'a3bc4d2'
+ * compress('aaabccccdd');
+ *
+ * @param {string} input - The original input string to be compressed.
+ * @returns {string} - The compressed string.
+ */
+function compress(input) {
     // Remove numbers from the original string
-    const noNumbers = input.replace(/[0-9]/g, '');
-
+    var noNumbers = input.replace(/[0-9]/g, '');
     // Start processing from the second character (because the first character initializes prevChar)
     return noNumbers[0] ? process(noNumbers.slice(1), noNumbers[0], 1) : '';
 }
-
 // Test cases
 console.log(compress('aaabccccdd') === 'a3bc4d2');
 console.log(compress('aaaaaffffffffffc') === 'a5f10c');
