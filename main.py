1b496e8a9f30231571fab461fed1b9f5cd0a6f63
Andrew Bettke <abettke@gmail.com>
2024-03-18 14:30:56 -0400

    Update documentation to include special character considerations.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def compress(input_str: str) -> str:
     """
     Compresses the input string by replacing repeated characters with the character followed by the count of the
     repetitions. Characters that do not repeat will not be followed by the count of repetitions. Additionally, the
-    function will strip numbers from the input string.
+    function will strip numbers and special characters from the input string.
     :param input_str: The string to be compressed.
     :return: The compressed string.
     """
