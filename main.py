d28f9df2d3147b9433ddb3592f22b688f7e9fb79
Andrew Bettke <abettke@gmail.com>
2024-03-18 13:58:59 -0400

    Rename function and add comment to describe function.

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def main():
-    print("Hello, World!")
+def compress(input_str: str) -> str:
+    """
+    Compresses the input string by replacing repeated characters with the character followed by the count of the
+    repetitions. Characters that do not repeat will not be followed by the count of repetitions. Additionally, the
+    function will strip numbers from the input string.
+    :param input_str: The string to be compressed.
+    :return: The compressed string.
+    """
+    return 'hello world'
+
 
 if __name__ == "__main__":
-    main()
+    print(compress('hello worldddddd'))
